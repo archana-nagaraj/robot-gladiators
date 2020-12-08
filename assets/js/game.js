c2298d92880da79f92fbed2577b794cb791b2bae
Archana Nagaraja <johndoe@example.com>
2020-12-08 05:55:15 -0800

    added more checks about player and enemy's hea;th after the attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,7 @@
     var fight = function(){
         // Alert players that they are starting the round
         window.alert("Welcome to Robot Gladiators!");
+
     };
 
     fight();
@@ -24,8 +25,24 @@
     // Log a message to the console to confirm it worked
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
-    // // Subtract the value of enemyAttack from the value of playerHealth, and use that result to update the value in the playerHealth variable.
-    // playerHealth = playerHealth - playerAttack;
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+      } 
+      else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      }
 
-    // // Log a resulting message to the console to confirm that it worked.
-    // console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    // Subtract the value of enemyAttack from the value of playerHealth, and use that result to update the value in the playerHealth variable.
+    playerHealth = playerHealth - playerAttack;
+
+
+    // Log a resulting message to the console to confirm that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining." );
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
